b5b3e4602350ff03127a61ad873d5df3a13462cd
thakur24 <thakurdhruv200@gmail.com>
2021-05-12 10:58:54 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { SingupComponent } from './singup/singup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { 
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  { 
+    path: 'home',
+    component: HomeComponent
+  },
+  { 
+    path: 'singup',
+    component: SingupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SingupComponent } from './singup/singup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SingupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
